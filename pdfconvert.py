6a33e432e057446f689d1c569a7bf6c69703074b
Rafael Macalaba <rafael.macalaba@thomsonreuters.com>
2018-05-11 22:23:44 +0800

    updating

--- pdfconvert.py
+++ pdfconvert.py
@@ -17,20 +17,21 @@ import csv
 
 
 class Converter(QMainWindow):
-    def __init__(self):
+    def __init__(self): #initialize the app
         super().__init__()
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle("pdftocsvconverter")
+        ''' This function is the main UI of the application '''
+        self.setWindowTitle("pdftocsvconverter") #setting the name of the app
         #self.setWindowIcon(QIcon(":/logo_tracker.png"))
         self.resize(280, 170)
   
         centralwidget = QWidget(self)
         
-
+        #adding push buttons
         self.btn_file = QPushButton("PDF File", self)
-        self.btn_file.clicked.connect(self.openFileNameDialog)
+        self.btn_file.clicked.connect(self.openFileNameDialog) #connecting methods/events to the buttons
 
         self.btn_convert = QPushButton("Convert", self)
         self.btn_convert.clicked.connect(self.Convert)
@@ -39,20 +40,20 @@ class Converter(QMainWindow):
         self.btn_showFile = QPushButton("Show File", self)
         self.btn_showFile.clicked.connect(self.showFile)
 
-        self.btn_getPages = QPushButton("Pages", self)
+        self.btn_getPages = QPushButton("Pages", self) 
         self.btn_getPages.clicked.connect(self.getPages)
 
-        self.cb_time_series = QCheckBox('Split Single Cell Values', self)
-        self.cb_euro_delim = QCheckBox('Euro Delimiter', self)
+        self.cb_time_series = QCheckBox('Split Single Cell Values', self) #checkbox for split single cell values feature.
+        self.cb_euro_delim = QCheckBox('Euro Decimal', self) #checkbox for euro feature.
         
         #self.cb_time_series.stateChanged.connect(self.state_changed)
         
     
 
 
-
+        #initializing layout
         grid = QGridLayout()
-
+        #adding the coordinates of each buttons and checkboxes
         grid.addWidget(self.btn_file, 1, 0)
         grid.addWidget(self.btn_convert, 3, 0)
         grid.addWidget(self.btn_showFile, 4, 0)
@@ -62,117 +63,145 @@ class Converter(QMainWindow):
         
         
 
-        self.statusBar()
+        self.statusBar() #adding a statusbar for converting files
 
-        centralwidget.setLayout(grid)
+        centralwidget.setLayout(grid) #adding grid to the setlayout method of centralwidget 
         self.setCentralWidget(centralwidget)
-        self.center()
-        self.show()
+        self.center() 
+        self.show() #show the UI
     
+    
+    usefile="" #initialize with an empty string
 
-    usefile=""
     def openFileNameDialog(self):    
-        options = QFileDialog.Options()
+        '''This function will handle the event for the PDF Files selection from the directory.'''
+        options = QFileDialog.Options() #setting options
         fileName, _ = QFileDialog.getOpenFileName(self,"Current Working Directory", "","PDF Files (*.pdf)", options=options)
         
         if fileName:
             global usefile
             usefile = fileName
-            QMessageBox.information(self, 'Message', "File used: %s" % usefile)
+            QMessageBox.information(self, 'Message', "File used: %s" % usefile) #adding information for the user
         #    self.getPages()
             #self.btn_convert.setEnabled(True)
-            self.make_directory()
+            return self.make_directory() #make the directory for the output file.
         
         else:
-            QMessageBox.information(self, 'Warning', "You did not select a .pdf file")
-            self.openFileNameDialog()
+            QMessageBox.information(self, 'Warning', "You did not select a .pdf file") #error handling of the .pdf file selection if the user doesn't picked a pdf file.
+            return self.openFileNameDialog() #return the openfilenamedialog when error comes
             
     def getPages(self):
+        '''This function will handle the pages to be used in converting the pdf table. '''
         global page
-        page, okPressed = QInputDialog.getText(self, "Message","Pages:", QLineEdit.Normal, "")
+        page, okPressed = QInputDialog.getText(self, "Message","Pages:", QLineEdit.Normal, "") #set user page specification.
         if okPressed and page != '':
-            QMessageBox.information(self, 'Message', "Pages used: %s" % page)
+            QMessageBox.information(self, 'Message', "Pages used: %s" % page) #return pages that are used.
         else:
-            self.getPages()
+            return self.getPages() #return the function loop if error comes.
 
     def showFile(self):
-        try:
-            if self.cb_time_series.isChecked():
+        '''This function will return different output files. '''
+        try: #show different files for different features used.
+            if self.cb_time_series.isChecked(): #if the split single cell values is ticked run the time_series function then return the log.csv file.
                 self.time_series()
-                os.startfile("C:/OutputFolder/log.csv")
+                return os.startfile("C:/OutputFolder/time_series_transformed.csv")
+            
+            elif self.cb_euro_delim.isChecked():
+                self.final_euro_delimiter()
+                return os.startfile("C:/OutputFolder/transformed_euro.csv")
                 
-            else:
-                os.startfile("C:/OutputFolder/Output.csv")
-                self.statusBar().showMessage('')
+            else: #return the normal output.csv file
+                self.statusBar().showMessage('') #show blank to remove the converted text in the statusbar
+                return os.startfile("C:/OutputFolder/Output.csv")
+                
             
 
-        except Exception as e:
-            QMessageBox.information(self, 'Warning', "%s" % e)
-
+        except: #exception handling, return information/hint for the user what to do.
+            QMessageBox.information(self, 'Warning', "Hint: Close the output file")
+    
     def Convert(self):
+        '''This function will handle the converting process of the pdf tables using the convert_into function of the tabula-py'''
         try:
             tabula.convert_into("%s" % usefile, output_path="C:/OutputFolder/Output.csv", output_format="csv", pages ="%s" % page)
-            self.statusBar().showMessage('Converted')
+            self.statusBar().showMessage('Converted') #show when the file is already converted
 
-        except:                        
-            QMessageBox.information(self, "WARNING", "Close the output file or Select the page!")
+        except:  #user hints for error handling                      
+            QMessageBox.information(self, "WARNING", "Hint:Close the output file or Select the page.")
 
 
     def center(self):
+        '''PyQt5 misc for frame geometry of UI'''
         qr = self.frameGeometry()
         cp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
     
-    
-    def make_directory(self):
-        directory = "C:/OutputFolder/"
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-    
-    def state_changed(self):
-        if self.cb_time_series.isChecked():
-            self.time_series()
-            
-        else:
-            try:
-                os.remove("C:/OutputFolder/log.csv")
-            except:
-                QMessageBox.information(self, "WARNING", "Hint: Close the output file")
+    def make_directory(self): 
+        '''This function will make a directory.'''
+        directory = "C:/OutputFolder/" #exact location of the directory.
 
-#HELLO ROXANNE :)
+        if not os.path.exists(directory): #if the directory doesnt exist, make a directory.
+            os.makedirs(directory)
+
+
     #FEATURES
+   
     def time_series(self):
-        filename= "C:/OutputFolder/Output.csv"
-        logfile = "C:/OutputFolder/log.csv"
-        if os.path.exists(logfile):
+        '''This function will solve the problem of the output file where there are multiple values in a single cell'''
+        filename= "C:/OutputFolder/Output.csv" #specify the output csv as input file
+        logfile = "C:/OutputFolder/time_series_transformed.csv" #specify the log.csv to be the second output file
+        if os.path.exists(logfile): #if the log.csv exists, remove it.
             os.remove(logfile)
-        with open("C:/OutputFolder/log.txt", "w", encoding="utf-8") as out_file:
-            with open(filename, "r") as in_file:
-                reader = csv.reader(in_file, quoting=csv.QUOTE_MINIMAL)
-                to_list = []
-                
+            
+        with open("C:/OutputFolder/time_series_transformed.txt", "w", encoding="utf-8") as out_file: #create an output file with a txtfile to be converted into csv
+            with open(filename, "r") as in_file: #load the Output.csv to be parsed and edited as a python object.
+                reader = csv.reader(in_file, quoting=csv.QUOTE_MINIMAL) #using reader method of the csv module to iterate over the items in a list.
+                #quote minimal is used to minimize quotation of the elements in the reader class.
+                to_list = [] #instantiate a list
                 for line in reader:
-                    to_list.append(" ".join(line) + "\n") #join items in a list using space                
-                to_str = []
-                
-                if self.cb_euro_delim.isChecked():                   
-                    for i in to_list:                                 
-                        to_str.append(i.replace(",",".")) #replace delimiter "," with blank to concatenate
-                else:
-                    for i in to_list:                                 
-                        to_str.append(i.replace(","," "))
-                    
+                    if self.cb_euro_delim.isChecked():
+                        to_list.append("_".join(line) + "\n")     
+                    else:
+                        to_list.append(" ".join(line) + "\n") #join items in a list using space                
+                to_str = []      
+                for i in to_list:
+                    if self.cb_euro_delim.isChecked():                                               
+                        to_str.append(i.replace(",",".").replace(" ","")) #replace delimiter "," with blank to concatenate
+                    else:                  
+                        to_str.append(i.replace(",",""))               
                 to_text = []
                 for text in to_str:
-                    to_text.append(text.replace(" ",",")) #replace spaces to the natural delimiter ","
+                    if self.cb_euro_delim.isChecked():  
+                        to_text.append(text.replace("_",","))
+                    else:
+                        to_text.append(text.replace(" ",",")) #replace spaces to the natural delimiter ","
                     #TO DO: use regex to find all text/dates/headers and then not include them on the text.replace(" ",",") exception.
                     #hint: text.replace(" ",",") if text not in headers
-            out_file.writelines(to_text)
-        os.rename("C:/OutputFolder/log.txt", "C:/OutputFolder/log.csv")
+            out_file.writelines(to_text) #write the to_text list in the log.txt file
+        os.rename("C:/OutputFolder/time_series_transformed.txt", "C:/OutputFolder/time_series_transformed.csv") #convert the textfile into csv file by renaming it.
 
-if __name__ == "__main__":
+    def final_euro_delimiter(self):
+        '''This function solves the problem of the european delimiter of comma (,) as period (.)'''
+        
+        transformed_file = "C:/OutputFolder/transformed_euro.csv"
+        filename= "C:/OutputFolder/Output.csv"
+        if os.path.exists(transformed_file): #if the log.csv exists, remove it.
+            os.remove(transformed_file)
+        with open("C:/OutputFolder/transformed_euro.txt", "w", newline="", encoding="utf-8") as out_file: #create an output file with a txtfile to be converted into csv
+            writer = csv.writer(out_file)
+            with open(filename, "r") as in_file: #load the Output.csv to be parsed and edited as a python object.
+                reader = csv.reader(in_file, quoting=csv.QUOTE_MINIMAL) #using reader method of the csv module to iterate over the items in a list.
+                #quote minimal is used to minimize quotation of the elements in the reader class.
+                to_list = [] #instantiate a list
+                
+                for line in reader:
+                    to_list.append([x.replace(",",".").replace(" ","") for x in line])
+                    
+            writer.writerows(to_list)
+        os.rename("C:/OutputFolder/transformed_euro.txt", "C:/OutputFolder/transformed_euro.csv")   
+
+if __name__ == "__main__": #run the program
     app = QApplication(sys.argv)
     win = Converter()
     sys.exit(app.exec_())
